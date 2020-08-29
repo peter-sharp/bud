4be33b97acad5d588a6e9fcbff6541474397d779
Peter Sharp <peter.victor.sharp@gmail.com>
2020-08-29 07:25:57 -0500

    Adds ability to edit budgets

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ Array.from(document.querySelectorAll('[budgeting-app]')).forEach(async function 
     const newBudgetButton = el.querySelector('[budgeting-app_new-budget-button]');
     const budgetList = el.querySelector('[budgeting-app_budgets]');
     const budgetItemTemplate = document.getElementById('budgeting_app_budget_item');
+    const budgetEditFormTemplate = document.getElementById('budgeting_app_edit_budget_form');
     const state = await db.collection('budgets', {
         state: null,
-        budgets: []
+        budgets: [],
+        budgetIndex: {}
     }).fetchAll();
+    state.budgetIndex = getBudgetIndex(state.budgets);
+
     render();
 
     newBudgetForm.addEventListener('submit', function handleNewBudget(ev) {
@@ -27,9 +31,56 @@ Array.from(document.querySelectorAll('[budgeting-app]')).forEach(async function 
                 state.budgets.push({ id, name, amount, remaining, items: [] })
                 break;
         }
+        state.budgetIndex = getBudgetIndex(state.budgets);
         db.collection('budgets').save(state);
         render();
     })
+
+    function getBudgetIndex(budgets) {
+        const budgetIndex = {};
+        for(let budget of budgets) {
+            budgetIndex[budget.id] = budget;
+        }
+        return budgetIndex;
+    }
+
+    delegate(el, '[budgeting-app_budget-item]', 'dblclick', function handleBudgetEdit(ev) {
+        editBudgetItem(this);
+    });
+    delegate(el, '[budget-item_edit-button]', 'click', function handleBudgetEdit(ev) {
+        editBudgetItem(this.closest('[budgeting-app_budget-item]'));
+    });
+
+    function editBudgetItem(budgetItem) {
+        const budgetForm = budgetEditFormTemplate.content.cloneNode(true).firstElementChild;
+
+        const budgetId = budgetItem.querySelector('[name="budgetId"]').value;
+        const budget = state.budgetIndex[budgetId];
+
+        // render
+        budgetForm.elements['name'].value = budget.name;
+        budgetForm.elements['amount'].value = budget.amount;
+
+        let budgetDisplay = document.createDocumentFragment();
+        budgetDisplay.append(...budgetItem.cloneNode(true).childNodes);
+        Array.from(budgetItem.childNodes).forEach(x => x.remove());
+
+        budgetItem.append(budgetForm);
+
+        budgetForm.addEventListener('submit', function saveBudget(ev) {
+            ev.preventDefault();
+            Array.from(budgetItem.childNodes).forEach(x => x.remove());
+            budget.name = this.elements['name'].value;
+            const spent = budget.amount - budget.remaining;
+            budget.amount = this.elements['amount'].value;
+            budget.remaining = budget.amount - spent;
+            db.collection('budgets').save(state);
+            // render
+            budgetItem.append(budgetDisplay);
+            budgetItem.querySelector('[budget-remaining]').innerText = round2dp(budget.remaining);
+            budgetItem.querySelector('[budget-amount]').innerText = round2dp(budget.amount);
+        });
+    }
 
     delegate(el, '[budget-items-form]', 'submit', function handleItemsUpdate(ev) {
         ev.preventDefault();
@@ -46,13 +97,10 @@ Array.from(document.querySelectorAll('[budgeting-app]')).forEach(async function 
                 items.push({name, amount});
             }
         });
-        let budget;
-        for(budget of state.budgets) {
-            if(budget.id != budgetId) continue;
-            budget.items = items;
-            budget.remaining = budget.amount - total;
-            break;
-        }
+        let budget = state.budgetIndex[budgetId];
+        budget.items = items;
+        budget.remaining = budget.amount - total;
+       
         db.collection('budgets').save(state);
         const budgetItem = this.closest('[budgeting-app_budget-item]');
         renderBudget(budgetItem, budget);
@@ -93,6 +141,7 @@ Array.from(document.querySelectorAll('[budgeting-app]')).forEach(async function 
         });
         budgetItem.querySelector('[budget-remaining]').innerText = round2dp(budget.remaining);
     }
+    
 })
 
 function getId() {
